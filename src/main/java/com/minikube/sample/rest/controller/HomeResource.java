772e715b9903fd0778c9e8abc961b485fbf93ab9
Eresh Gorantla <eresh.zealous@gmail.com>
2018-12-11 20:45:41 +0530

    Initial commit

---
+++ src/main/java/com/minikube/sample/rest/controller/HomeResource.java
@@ -0,0 +1,39 @@
+package com.minikube.sample.rest.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Gorantla, Eresh
+ * @created 06-12-2018
+ */
+@RestController
+@RequestMapping("/home")
+public class HomeResource {
+
+
+    @GetMapping("/data")
+    public ResponseEntity<ResponseData> getData() {
+        ResponseData responseData = new ResponseData();
+        responseData.setId(1);
+        responseData.setName("Eresh");
+        responseData.setPlace("Hyderabad");
+        return new ResponseEntity<ResponseData>(responseData, HttpStatus.OK);
+    }
+
+    @Getter
+    @Setter
+    public class ResponseData {
+        private String name;
+        private Integer id;
+        private String place;
+    }
+}
